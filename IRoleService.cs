d726799ceffd2b03e571fc82b0c22085b73dc8ea
Amit25-v <akumaramit9927@gmail.com>
2026-04-03 12:06:38 +0530

    Add files via upload
    i done the Add Role and Get the Role Services

---
+++ IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using DataAcessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.RoleService
+{
+    public interface IRoleService
+    {
+        public void AddRole(RoleviewModels role);
+        public List<RoleviewModels> ListRole();
+    }
+}
---
+++ RoleService.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using DataAcessLayer;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BusinessLayer.RoleService
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SqlConnection _connection;
+        public RoleService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public void AddRole(RoleviewModels role)
+        {
+            try
+            {
+                DynamicParameters parm = new DynamicParameters();
+                parm.Add("@RoleName", role.RoleName);
+                _connection.Execute("CreateRole", parm, commandType: CommandType.StoredProcedure);
+
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public List<RoleviewModels> ListRole()
+        {
+            try
+            {
+                if (_connection.State == ConnectionState.Closed)
+                {
+                    _connection.Open();
+                }
+
+                    var result = _connection.Query<RoleviewModels>(
+                        "ListRoles",
+                        commandType: CommandType.StoredProcedure
+                    ).ToList();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+    }
+
+    }
