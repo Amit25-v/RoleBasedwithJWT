5a55b089aa84e497e9d2325eb6282cf6f2ee7056
Amit25-v <akumaramit9927@gmail.com>
2026-04-03 12:13:00 +0530

    Add files via upload
    i done the Table for Employee Table

---
+++ EmployeeTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Protocol;
+using Sieve.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAcessLayer
+{
+    public class EmployeeTable
+    {
+        public int EmployeeId { get; set; }
+
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string? Addrees { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string? Email_Id { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string? City { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6)]
+        public string? Password { get; set; }
+
+        public int RoleId { get; set; }
+
+        // ✅ Navigation Property
+        public RoleTable? Role { get; set; }
+    }
+}
---
+++ EmployeeView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAcessLayer
+{
+    public class EmployeeView
+    {
+        public int EmployeeId { get; set; }
+
+        public string? Name { get; set; }
+        public string? Addrees { get; set; }
+        public string? Email_Id { get; set; }
+        public string? City { get; set; }
+        public string ? Password { get; set; }
+        public int RoleId { get; set; }
+        public string? RoleName { get; set; }
+    }
+}
